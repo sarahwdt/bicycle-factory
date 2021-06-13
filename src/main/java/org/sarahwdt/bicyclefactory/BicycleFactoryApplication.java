6cdc19c709443fd8ac20898a77923c1b2b56a2a6
Sarah <sarahwilldietoday@gmail.com>
2021-06-13 16:49:14 +0400

    Initial commit

---
+++ src/main/java/org/sarahwdt/bicyclefactory/BicycleFactoryApplication.java
@@ -0,0 +1,13 @@
+package org.sarahwdt.bicyclefactory;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BicycleFactoryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BicycleFactoryApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/sarahwdt/bicyclefactory/BicycleFactoryApplicationTests.java
@@ -0,0 +1,13 @@
+package org.sarahwdt.bicyclefactory;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BicycleFactoryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
